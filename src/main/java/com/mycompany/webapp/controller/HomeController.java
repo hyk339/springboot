2a0cef8a9152fa185575b5f2d3fa602a78c40906
hyk339 <hyk339@gmail.com>
2021-10-16 19:38:54 +0900

    최초 프로젝트 공유 파일

---
+++ src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.mycompany.webapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@Log
+public class HomeController {
+	
+	
+	@RequestMapping("/")
+	public String home() {
+		log.info("실행");
+		return "home";
+	}
+}
